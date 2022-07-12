27ea15cfdec9f4adeb2d86230c7a672934a34ee3
BiancaLopresto <bianlopre19@gmail.com>
2022-07-12 20:02:59 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1DeBiancaLopresto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class ejercicio1debiancalopresto
+{
+
+	static void Main(string[] args)
+	{
+		double multiplicacion;
+		double numero1;
+		double resultado;
+		Console.Write("Ingrese los dolares: ");
+		numero1 = Double.Parse(Console.ReadLine());
+		multiplicacion = numero1 * 230;
+		Console.WriteLine("Cantidad en pesos en el mercado oficial: " + multiplicacion);
+		multiplicacion = numero1 * 280;
+		Console.WriteLine("Cantidad de pesos en el mercado paralelo: " + multiplicacion);
+		resultado = multiplicacion * 1.17;
+		Console.WriteLine("Cantidad en euros con el dolar en mercado oficial: " + resultado);
+		resultado = numero1 * 1.17;
+		Console.WriteLine("Cantidad en euros con el dolar en mercado paralelo: " + resultado);
+	}
+
+}
+
+
+
+
